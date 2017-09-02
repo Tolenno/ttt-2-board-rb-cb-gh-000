05d1182522ebcb81403c0f09dc956d046cb0a3c1
Matt Hough <matt@hough.tv>
2017-09-02 21:37:04 +0000

    automatically backed up by learn

--- hello.rb
+++ hello.rb
@@ -8,4 +8,12 @@ end
 
 puts say_hello_five
 
+def bob_likes_onion
+  puts "I am Bob,"
+  puts "I like onion very much."
 
+  puts "Can you tell me where the"
+  puts "closest onion shop is?"
+
+  puts "Thanks!"
+end
